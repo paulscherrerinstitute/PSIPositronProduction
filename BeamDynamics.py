d4669921759d677421f507019c2a28dbe7d5dd67
Mattia Schaer <mattia.schaer@psi.ch>
2021-08-19 15:46:47 +0200

    Simulation database in json format, can be exported to Excel file.

--- BeamDynamics.py
+++ BeamDynamics.py
@@ -1,4 +1,7 @@
+import os
 import numpy as np
+import pandas as pd
+import json
 
 
 
@@ -7,6 +10,14 @@ E_REST_ELECTRON = 0.5100   # Rest energy of the electron [MeV]
 
 
 def z_to_t(z, pz, Erest):
-    beta = np.sqrt(pz**2. / (pz**2. + (Erest/c)**2.))
-    t = z / (beta*c)
+    beta = np.sqrt(pz**2. / (pz**2. + (Erest/C)**2.))
+    t = z / (beta*C)
     return t
+
+
+def export_sim_db_to_xlsx(jsonFilePath):
+    dbFile = open(jsonFilePath)
+    dbJson = json.load(dbFile)
+    dbDf = pd.json_normalize(dbJson)
+    xlsxFilePath = os.path.splitext(jsonFilePath)[0] + '.xlsx'
+    dbDf.to_excel(xlsxFilePath)
