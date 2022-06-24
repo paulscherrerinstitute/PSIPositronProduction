59c389cebde20e1bdc63eeaffbe2732c4e306109
Mattia Schaer <mattia.schaer@psi.ch>
2022-06-24 10:09:48 +0200

    Corresponding changes to this other simulation. It might not run through at the moment.

--- RFTrack/Run_Linac1_Section1_Simple.py
+++ RFTrack/Run_Linac1_Section1_Simple.py
@@ -7,28 +7,7 @@ import matplotlib.pyplot as plt
 import json
 
 
-def load_fieldmap_rf_clic(fieldmapBasePath):
-    meshAxes = ['ra', 'ta', 'za']
-    meshes = {}
-    for meshAx in meshAxes:
-        meshes[meshAx] = np.loadtxt(
-            fieldmapBasePath+'_'+meshAx+'.dat', skiprows=5
-        )
-    matDimensions = [meshes[meshAx].shape[0] for meshAx in reversed(meshAxes)]
-    complexComponents = ['Ex', 'Ey', 'Ez', 'Bx', 'By', 'Bz']
-    rfFields = {}   
-    for complComp in complexComponents:
-        rfFields[complComp] = np.loadtxt(
-            fieldmapBasePath+'_'+complComp+'.dat', skiprows=5, dtype=np.complex128,
-            converters={0: lambda s: np.fromstring(
-                s.decode("latin1").replace('(','').replace(')',''),sep=','
-            ).view(np.complex128)}
-        )
-        rfFields[complComp] = rfFields[complComp].reshape(matDimensions).transpose() #.astype(np.complex128)
-    return rfFields, meshes
-
-
-### USER INPUT START ###
+# USER INPUT START ###
 
 # BUNCH_TYPE = 'Gaussian'
 #
@@ -38,12 +17,14 @@ REL_PATH = 'SimulationRuns/DistrsFromExternalPartners/PositronsAt200MeV/YongkeDi
 # FILE_NAME = 'CTSB-N02-F100-E06-S0.5-T5.0_FCTest_Pavel_SolC_CLICTW-OptionA08B7-Bc0.50.dat'
 # FILTER_SPECS_SELECTOR = ''
 FILE_NAME = 'CTSB-N02-F100-E06-S0.5-T5.0_HTSTest_JNov04_SolC_CLICTW-Ztc200-Ri15-Bc0.50.dat'
-FILTER_SPECS_SELECTOR = 'CTSB-N02-F100-E06-S0.5-T5.0_HTSTest_JNov04_SolC_CLICTW-Ztc200-Ri15-Bc0.50_MainBunch'
+FILTER_SPECS_SELECTOR = \
+    'CTSB-N02-F100-E06-S0.5-T5.0_HTSTest_JNov04_SolC_CLICTW-Ztc200-Ri15-Bc0.50_MainBunch'
 # FILE_NAME = 'CTSB-N02-F100-E06-S0.5-T5.0_HTSTest_JNov04_SolC_PSISW-Ztc200-Ri15-Bc1.50.dat'
-# FILTER_SPECS_SELECTOR = 'CTSB-N02-F100-E06-S0.5-T5.0_HTSTest_JNov04_SolC_PSISW-Ztc200-Ri15-Bc1.50_MainBunch'
+# FILTER_SPECS_SELECTOR = \
+#     'CTSB-N02-F100-E06-S0.5-T5.0_HTSTest_JNov04_SolC_PSISW-Ztc200-Ri15-Bc1.50_MainBunch'
 RFTRACK_FORMAT = 'rftrack_xp_t'
 
-R_APERTURE = 20e-3   # [mm]
+R_APERTURE = 20e-3   # [m]
 
 # SOLENOID_TYPE = 'HomogeneousChannel'
 # BZ_SOLENOID = 0.5   # [T]
@@ -54,6 +35,9 @@ SOLENOID_TYPE = 'Analytical'
 BZ_SOLENOID = 0.5   # [T]
 R_IN_SOLENOID = 0.100   # [m]
 SEPARATION_SOLENOID = 0.3   # [m]
+# TODO: Give better names to the two following variables
+FILE_PATH = None
+BZ_CORR_FACTOR = None
 #
 # SOLENOID_TYPE = 'SimulatedFieldmap'
 # FILE_PATH = '/afs/psi.ch/project/Pcubed/SimulationRuns/Fieldmaps/custom_solenoid_n_9_cavity.txt'
@@ -69,19 +53,18 @@ SEPARATION_SOLENOID = 0.3   # [m]
 #
 AUTOPHASING = False
 RF_TYPE = 'field_map_CLIC_Lband'
-RF_FREQ = 1.9986163867e+09   # [Hz]
+RF_FIELDMAP_DIM = '3D'
 RF_N_CELLS = 30
 RF_N_PERIODS_PER_STRUCTURE = np.round(RF_N_CELLS/3)
-RF_L_STRUCTURE = RF_N_PERIODS_PER_STRUCTURE * bd.C / RF_FREQ   # [m]
-RF_N_STRUCTURES  = 58 #58
+RF_L_STRUCTURE = RF_N_PERIODS_PER_STRUCTURE * bd.C / rfttools.RF_CLIC_FREQ   # [m]
+RF_N_STRUCTURES = 2  # 58
 RF_GRADIENT = 15.0E6   # [V/m]
-RF_GRADIENT_FIELDMAP = 11.23E6   # [V/m]
-RF_T0 = -70./360. / RF_FREQ * bd.C * 1e3   # [mm/c]
+RF_T0 = -70./360. / rfttools.RF_CLIC_FREQ * bd.C * 1e3   # [mm/c]
 RF_PHASE = 0   # [deg]
 RF_SEPARATION = 0.2   # [m]
 
 RF_HARMONIC_ON = False
-RF_HARMONIC_FREQ = 3 * RF_FREQ
+RF_HARMONIC_FREQ = 3 * rfttools.RF_CLIC_FREQ
 RF_HARMONIC_PHASE_ADV = 2./3.*np.pi   # [rad]
 RF_HARMONIC_N_CELLS = 120
 RF_HARMONIC_GRADIENT = 30e6   # [V/m]
@@ -94,7 +77,7 @@ if RF_HARMONIC_ON:
     N_SOLENOIDS += 1
 # N_SOLENOIDS = int(E_GAIN_LINAC/RF_GRADIENT/SPACING_SOLENOID)
 
-### USER INPUT END ###
+# USER INPUT END ###
 
 
 if BUNCH_TYPE == 'Gaussian':
@@ -139,7 +122,9 @@ elif BUNCH_TYPE == 'FromRFTrack':
         filterSpecs = filterSpecsList[FILTER_SPECS_SELECTOR]['filterSpecs']
         refParticle = filterSpecsList[FILTER_SPECS_SELECTOR]['RefParticle1']
         beamIn = bd.filter_distr(beamIn, filterSpecs)
-    M0 = bd.convert_standard_df_to_rftrack(standardDf=beamIn, rftrackDfFormat=RFTRACK_FORMAT)[0].to_numpy()
+    M0 = bd.convert_standard_df_to_rftrack(
+        standardDf=beamIn, rftrackDfFormat=RFTRACK_FORMAT
+    )[0].to_numpy()
     BUNCH_POPULATION = M0.shape[0]
     B06d = rft.Bunch6d(PARTICLE_MASS, BUNCH_POPULATION, PARTICLE_CHARGE, M0)
     B0 = rft.Bunch6dT(B06d)
@@ -174,8 +159,8 @@ if SOLENOID_TYPE == 'Analytical':
     solFieldmapStep = zAxis[1] - zAxis[0]
 elif SOLENOID_TYPE == 'SimulatedFieldmap':
     solMatrix = np.loadtxt(FILE_PATH)
-    solFieldmapStep = solMatrix[1,0] - solMatrix[0,0]
-    solBzOnAxis = BZ_SOLENOID * BZ_CORR_FACTOR * solMatrix[:,1]
+    solFieldmapStep = solMatrix[1, 0] - solMatrix[0, 0]
+    solBzOnAxis = BZ_SOLENOID * BZ_CORR_FACTOR * solMatrix[:, 1]
 
 
 if RF_TYPE == 'HomogeneousAccel':
@@ -183,56 +168,34 @@ if RF_TYPE == 'HomogeneousAccel':
     drift = rft.Drift(L_DRIFT)
     drift.set_static_Efield(0, 0, RF_GRADIENT)
     if SOLENOID_TYPE == 'HomogeneousChannel':
-            drift.set_static_Bfield(0, 0, BZ_SOLENOID)
+        drift.set_static_Bfield(0, 0, BZ_SOLENOID)
 else:
-    # Fields of 1 RF period (3 cells)
-    # TODO: Data location and path construction
-    rfFields, rfMesh = load_fieldmap_rf_clic('./RFTrack/YongkeTool/field/'+RF_TYPE)
-    print('Setting up fieldmap...')
-    # 3D
-    # rf = rft.RF_FieldMap(
-    #     rfFields['Ex'], rfFields['Ey'], rfFields['Ez'],
-    #     rfFields['Bx'], rfFields['By'], rfFields['Bz'],
-    #     rfMesh['ra'][0], rfMesh['ta'][0],
-    #     rfMesh['ra'][1] - rfMesh['ra'][0],
-    #     rfMesh['ta'][1] - rfMesh['ta'][0],
-    #     rfMesh['za'][1] - rfMesh['za'][0],
-    #     rfMesh['za'][-1],
-    #     RF_FREQ, +1
-    # )
-    # rf.set_cylindrical(True)
-    # 1D
-    rf = rft.RF_FieldMap_1d(rfFields['Ez'][0,0,:], rfMesh['za'][1] - rfMesh['za'][0], rfMesh['za'][-1], RF_FREQ, +1)
-    print('... done!')
-    # rf.set_aperture(R_APERTURE, R_APERTURE, 'circular')
-    # RF.set_odeint_algorithm("rkf45");
-    # RF.set_nsteps(50);
-    # RF.set_aperture(20e-3, 20e-3, "circular"); % meter
+    rf = rfttools.rf_clic_single_period(RF_FIELDMAP_DIM)
     rfGap = rft.Drift(RF_SEPARATION)
-    # RF_GAP.set_odeint_algorithm("rkf45");
-    # RF_GAP.set_nsteps(50);
-    # RF_GAP.set_aperture(0.1, 0.1, "circular"); % meter
     if SOLENOID_TYPE == 'HomogeneousChannel':
         rf.set_static_Bfield(0, 0, BZ_SOLENOID)
-        rfGap.set_static_Bfield(0,0, BZ_SOLENOID)
+        rfGap.set_static_Bfield(0, 0, BZ_SOLENOID)
     lat = rft.Lattice()
     # Initial gap
     INITIAL_RF_GAP_L = (L_SOLENOID + SEPARATION_SOLENOID + RF_SEPARATION) / 2.
     initialRfGap = rft.Drift(INITIAL_RF_GAP_L)
     tRf = RF_T0 - INITIAL_RF_GAP_L*1e3
+    # TODO: Check double drift space at the beginning
     lat.append(initialRfGap)
     for structInd in np.arange(RF_N_STRUCTURES):
         lat.append(rfGap)
         rf.set_phid(RF_PHASE)
-        rf.set_P_actual((RF_GRADIENT/RF_GRADIENT_FIELDMAP)**2.)
+        rf.set_P_actual((RF_GRADIENT/rfttools.RF_CLIC_GRADIENT)**2.)
         rf.set_t0(tRf)
-        tRf += RF_SEPARATION * 1e3   # [mm/c]
         for rfPeriodInd in np.arange(RF_N_PERIODS_PER_STRUCTURE):
             lat.append(rf)
+        tRf += RF_SEPARATION * 1e3   # [mm/c]
 
 if RF_HARMONIC_ON:
     # TODO: Document this
-    # By N_CELLS: - = TW starts at the beginning of the cell, + = TW starts in the middle of the cell (useful for couplers)
+    # By N_CELLS:
+    # - = TW starts at the beginning of the cell
+    # + = TW starts in the middle of the cell (useful for couplers)
     RF_HARMONIC_MAIN_MODE_INDEX = 0
     rfHarmonic = rft.TW_Structure(
         np.array([RF_HARMONIC_GRADIENT]), RF_HARMONIC_MAIN_MODE_INDEX,
@@ -246,6 +209,7 @@ if RF_HARMONIC_ON:
 
 # Tracking volume
 vol = rft.Volume()
+# TODO: Is this a circular aperture?
 vol.set_aperture(R_APERTURE)
 if RF_TYPE == 'HomogeneousAccel':
     vol.add(drift, 0, 0, 0)
@@ -253,7 +217,7 @@ else:
     vol.add(lat, 0, 0, 0)
 if not SOLENOID_TYPE == 'HomogeneousChannel':
     solenoid = rft.Static_Magnetic_FieldMap_1d(solBzOnAxis, solFieldmapStep)
-    for solInd in range(0,N_SOLENOIDS):
+    for solInd in range(0, N_SOLENOIDS):
         vol.add(solenoid, 0, 0, solInd*SPACING_SOLENOID, 'center')   # element, X, Y, Z in [m]
 
 print('Setup complete, going to track...')
@@ -282,26 +246,11 @@ M1 = B1.get_phase_space()   # x [mm], Px [MeV/c], y [mm], Py [MeV/c], S [mm], Pz
 # TODO: What was this?
 # B1S = vol.get_bunch_at_s1()
 
-# Compute capture efficiency
-Mlost = B1.get_lost_particles()   # Columns 1-6 like M1, t [mm/c] at which particle was lost, m [kg], Q [?] of particle type, Q of macro-particle [?]
-Mlost = Mlost[Mlost[:, 4].argsort()]
-sCapture = Mlost[:, 4]
-captureEff = (M0.shape[0] - np.arange(1, Mlost.shape[0]+1, 1)) / M0.shape[0]
-
-TT = vol.get_transport_table('%mean_S %emitt_x %emitt_y %emitt_4d %sigma_X %sigma_Y %mean_E')
 beamOut, _ = bd.convert_rftrack_to_standard_df(
     rftrackDf=M1, rftrackDfFormat='rftrack_Px_S', t=B1.t/bd.C*1e6,
     pdgId=-11, Qbunch=Q_DRIVE_BEAM*M1.shape[0]/N_MACROPARTICLES_DRIVE_BEAM,
     outFwfPath='./DistrOut_Linac1_Section1_Simple'
 )
-
-# Prepare Bz for plotting
-zAxis = np.linspace(vol.get_s0(), vol.get_s1(), 1000)   # [m]
-Bz = []
-for z in zAxis:
-    [E, B] = vol.get_field(0, 0, z*1e3, 0)   # x,y,z,t (mm, mm/c)
-    Bz.append(B[2])
-Bz = np.array(Bz)
 
 plotDefs = [
     {
@@ -329,41 +278,8 @@ plotDefs = [
 ]
 ax = bd.plot_distr([beamIn, beamOut], plotDefs)
 
-sLims = TT[[0, -1], 0] / 1e3
-Elims = [0, TT[-1,6]]
 fig2, ax2 = plt.subplots(5, 1)
-ax2[0].plot(zAxis, Bz)
-ax2[0].set_xlim(sLims)
-ax2[0].set_xlabel('s [m]')
-ax2[0].set_ylabel('Bz [T]')
-ax2[0].grid()
-ax2[1].plot(TT[:,0]/1e3, TT[:,6])
-ax2[1].set_xlim(sLims)
-ax2[1].set_ylim(Elims)
-ax2[1].set_xlabel('s [m]')
-ax2[1].set_ylabel('Beam energy [MeV]')
-ax2[1].grid()
-ax2[2].plot(sCapture/1e3, captureEff)
-ax2[2].set_xlim(sLims)
-ax2[2].set_ylim([0, 1])
-ax2[2].set_xlabel('s [m]')
-ax2[2].set_ylabel('Capture efficiency')
-ax2[2].grid()
-ax2[3].plot(TT[:,0]/1e3, TT[:,1])
-ax2[3].plot(TT[:,0]/1e3, TT[:,2])
-ax2[3].plot(TT[:,0]/1e3, TT[:,3])
-ax2[3].set_xlim(sLims)
-ax2[3].set_xlabel('s [m]')
-ax2[3].set_ylabel('emitt [pi mm mrad]')
-ax2[3].legend(['emitt x', 'emitt y', 'emitt 4d'])
-ax2[3].grid()
-ax2[4].plot(TT[:,0]/1e3, TT[:,4])
-ax2[4].plot(TT[:,0]/1e3, TT[:,5])
-ax2[4].set_xlim(sLims)
-ax2[4].set_xlabel('s [m]')
-ax2[4].set_ylabel('sigma [mm]')
-ax2[4].legend(['sigma x', 'sigma y'])
-ax2[4].grid()
+rfttools.plot_transport(ax2[1:], vol, B0, B1)
 
 # # plt.ion()
 plt.show(block=False)
